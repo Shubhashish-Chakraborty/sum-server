3aa9e7b3bd61cb4d87fa8b270aa442470e97a4fe
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-07-22 23:37:36 +0530

    Added New Route , generating random quotes

--- index.js
+++ index.js
@@ -85,4 +85,52 @@ app.get("/notifications", (req, res) => {
 
 })
 
+const QUOTES = [{
+  id: 1,
+  quote: "Opportunity does not knock, it presents itself when you beat down the door."
+}, 
+{
+  id: 2,
+  quote: "No bird soars too high if he soars with his own wings."
+}, 
+{
+  id: 3,
+  quote: "I am an optimist. It does not seem too much use being anything else."
+}, 
+{
+  id: 4,
+  quote: "The only way to do great work is to love what you do."
+}, 
+{
+  id: 5,
+  quote: "Success is not the key to happiness. Happiness is the key to success."
+}, 
+{
+  id: 6,
+  quote: "Believe you can and you're halfway there."
+}, 
+{
+  id: 7,
+  quote: "Act as if what you do makes a difference. It does."
+}, 
+{
+  id: 8,
+  quote: "The future belongs to those who believe in the beauty of their dreams."
+}, 
+{
+  id: 9,
+  quote: "Don't watch the clock; do what it does. Keep going."
+}, 
+{
+  id: 10,
+  quote: "You are never too old to set another goal or to dream a new dream."
+}]
+
+app.get("/randomquote" , (req , res) => {
+  const randomIndex = Math.floor(Math.random() * QUOTES.length);
+  const randomQuote = QUOTES[randomIndex];
+
+  res.json(randomQuote);
+})
+
 app.listen(8080)
